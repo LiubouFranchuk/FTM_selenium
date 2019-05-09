ba517ddd48ceeb6a8f0cec9d4f92fee2abc43fcb
LiubouFranchuk <franchuklubov@gmail.com>
2019-05-09 12:37:25 +0300

    GoogleCloud - I tried, but failed

--- src/test/java/com/epam/ftm/Pages/CloudGooglePage.java
+++ src/test/java/com/epam/ftm/Pages/CloudGooglePage.java
@@ -1,10 +1,17 @@
 package com.epam.ftm.Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.logging.Logger;
 
 public class CloudGooglePage extends BasicPage{
 
     public static String cloudGoogleUrl = "https://cloud.google.com";
+
+    protected By instances = By.xpath("//label[contains(text(),'Number of instances')]/../input");
 
 
     public void accessUrl(){
@@ -16,12 +23,43 @@ public class CloudGooglePage extends BasicPage{
     }
 
     public void selectGoogleCloudPlatform(String platform){
+        driver.switchTo().frame(0);
         driver.findElement(By.xpath("//span[contains(text(),'" + platform + "')]/..")).click();
+        //TODO platform selection doesn't work - incorrect frame #? etc? I have no clue
     }
 
-    public void selectSubPlatform(String subPlatform){
-        driver.findElement(By.xpath("//div[@title='" + subPlatform + "']")).click();
-        //TODO - to implement. Elements can't be found. Probably frames issue
+    public void selectSubPlatform(String subPlatform) throws Exception {
+        driver.switchTo().frame(0);
+        WebElement dropdown = driver.findElement(By.id("input-0"));
+        dropdown.click();
+
+        int seqNum;
+        if (subPlatform == "Compute Engine"){
+            seqNum = 1;
+        } else if (subPlatform == "App Engine"){
+            seqNum = 2;
+        } else {
+            throw new Exception("Requested option is not available.");
+        }
+
+        WebElement option = driver.findElement(By.xpath("//ul[@id='ul-0']/li["+ seqNum + "]"));
+        option.click();
+
+        //TODO subplatform selection doesn't work - tried frames, action builder, etc. See Task3 - smoke1
+    }
+
+
+    public void setInstances(String num){
+        driver.switchTo().frame(0);
+        WebElement elm = driver.findElement(instances);
+        elm.click();
+        elm.sendKeys(num);
+        //TODO - frame0 no longer works for this page
+
+    }
+
+    public void method(){
+
     }
 
 
--- src/test/java/com/epam/ftm/Tests/Task3.java
+++ src/test/java/com/epam/ftm/Tests/Task3.java
@@ -1,34 +1,56 @@
 package com.epam.ftm.Tests;
 
 import com.epam.ftm.Pages.CloudGooglePage;
-import com.epam.ftm.Pages.PasteBinPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Task3 extends BasicTest  {
 
     @Test
-    public void smoke(){
-        driver.get("https://pastebin.com/pfp5CUBk");
-        WebElement elmToCheck = driver.findElement(By.xpath("//span[contains(text(),'git push')]"));
-        String color = elmToCheck.getCssValue("color");
-        Assert.assertEquals("#c20cb9", color);
-
-    }
-
-    @Test
-    public void Task3Test(){
+    public void Task3Test() throws Exception {
 
         CloudGooglePage cloudGooglePage = new CloudGooglePage();
         cloudGooglePage.accessUrl();
         cloudGooglePage.goToPage("Products");
         cloudGooglePage.goToPage("Pricing");
-        cloudGooglePage.selectGoogleCloudPlatform("Calculator");
-        cloudGooglePage.selectSubPlatform("Compute Engine"); //NOT working so far
+        cloudGooglePage.selectGoogleCloudPlatform("Calculators");
+        cloudGooglePage.selectSubPlatform("Compute Engine");
+        cloudGooglePage.setInstances("4");
+
+    }
 
 
+    @Test
+    public void smoke1() {
+        driver.get("https://cloud.google.com/products/calculator/");
+
+        driver.switchTo().frame(0);
+        WebElement dropdown = driver.findElement(By.id("input-0"));
+        dropdown.click();
+        new Actions(driver).moveToElement(driver.findElement(By.xpath("//ul[@id='ul-0']/li[3]"))).click().perform();
+
+
+//        CloudGooglePage cloudGooglePage = new CloudGooglePage();
+//        cloudGooglePage.selectSubPlatform("App Engine");
+
+//        driver.switchTo().frame(0);
+//        driver.findElement(By.id("input-0")).click();
+//        driver.findElement(By.xpath(""));
+
+//        new Actions(driver).moveToElement(driver.findElement(By.xpath("//span[contains(text(),'App Engine')]/../../.."))).click().perform();
+//        new Actions(driver).moveToElement(driver.findElement(By.xpath("//label[contains(text(),'Number of instances')]/../input"))).click().perform();
+
+    }
+
+
+    @Test
+    public void smoke2() {
+        driver.get("https://cloud.google.com/products/calculator/");
+
+        CloudGooglePage cloudGooglePage = new CloudGooglePage();
+        cloudGooglePage.setInstances("4");
 
     }
 
